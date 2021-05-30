d64e7b2534631e0add415b40f1a584f9af3dd32b
otodashinayo <naoto_yoshida@keio.jp>
2021-05-30 16:45:38 +0900

    00-09

--- ch01/09.py
+++ ch01/09.py
@@ -5,7 +5,8 @@ def main():
     res = []
     for t in s.split():
         if len(t) > 4:
-            res.append(t[0] + "".join(random.sample([u for u in t[1:-1]], len(t) - 2)) + t[-1])
+            res.append(
+                t[0] + "".join(random.sample([u for u in t[1:-1]], len(t) - 2)) + t[-1])
         else:
             res.append(t)
     res = " ".join(res)
